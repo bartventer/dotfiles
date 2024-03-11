843d5954d50de7990fd62c687e252a390bbb583f
Bart <bartventer@outlook.com>
2024-03-11 16:24:56 +0200

    fix(init.sh): add CI runner compatibility

--- init.sh
+++ init.sh
@@ -4,11 +4,15 @@
 source "$(dirname "$0")/scripts/log.sh"
 
 log_info "Initializing..."
-
-if [ "$(uname)" == "Darwin" ]; then
+if [ "${CI}" == "true" ]; then
+    REPO_DIR="${GITHUB_WORKSPACE}"
+elif [ "$(uname)" == "Linux" ]; then
+    REPO_DIR="$(dirname "$(realpath "$0")")"
+elif [ "$(uname)" == "Darwin" ]; then
     REPO_DIR="$(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")"
 else
-    REPO_DIR="$(dirname "$(realpath "$0")")"
+    log_error "Unsupported OS"
+    exit 1
 fi
 export REPO_DIR
 
