8064935431818dfa204367989e435954395d6226
Bart <bartventer@outlook.com>
2024-03-11 16:16:36 +0200

    feat(init.sh): add macOS compatibility for script directory resolution
    This commit enhances the initialization process in the init.sh script by adding support for macOS. It now checks the operating system and uses the appropriate command to resolve the script directory. This update ensures compatibility with both Linux and macOS systems.

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Source the log script file
 # shellcheck disable=SC1091
 source "$(dirname "$0")/scripts/log.sh"
 
 log_info "Initializing..."
 
-REPO_DIR="$(dirname "$(realpath "$0")")"
+if [ "$(uname)" == "Darwin" ]; then
+    REPO_DIR="$(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")"
+else
+    REPO_DIR="$(dirname "$(realpath "$0")")"
+fi
 export REPO_DIR
 
 log_success "Successfully initialized."
