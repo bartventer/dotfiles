54f033d39becdbb5cb632dfaf7e4ac0d3e7810ef
Bart <bartventer@outlook.com>
2024-03-31 18:53:03 +0200

    refactor(fonts_update.sh): remove functions

--- update_fonts.sh
+++ update_fonts.sh
@@ -1,78 +1,85 @@
 #!/bin/bash
 
 # update_fonts.sh
-# 
+#
 # Description: This script updates the fonts for the system. It checks for necessary dependencies, sets up a Python virtual environment, installs requirements, and fetches the fonts.
-# 
-# Functions:
-#   check_dependency(dep): Checks if a dependency is installed. If not, logs an error message and exits the script.
-#   setup_venv(): Sets up a Python virtual environment if it doesn't already exist.
-#   install_requirements(): Installs the Python requirements from a requirements.txt file.
-#   fetch_fonts(): Runs a Python script to fetch the fonts.
-# 
+#
 # Globals:
-#   VENV_DIR: The directory for the Python virtual environment.
-#   PYTHON_PATH: The path to the Python interpreter in the virtual environment.
-# 
+#   DOTFILES_DIR: The directory containing the dotfiles.
+#   DOTFILES_REQUIREMENTS: The path to the Python requirements file.
+#   FETCH_FONTS_SCRIPT: The path to the Python script that fetches the fonts.
+#   VENVS_DIR: The directory for Python virtual environments.
+#   DOTFILES_VENV_DIR: The directory for the Python virtual environment for this script.
+#   PYTHON_CMD: The command to run Python.
+#   DOTFILES_VENV_ACTIVATE: The path to the script that activates the Python virtual environment.
+#   PIP_CMD: The command to run pip.
+#
 # Usage:
 #   Run this script to update the fonts. For example:
 #     ./update_fonts.sh
-# 
-# Note:
-#   This script sources a script called init.sh, which should be in the same directory.
+#
 
 set -e
 
 # Initialization
 # shellcheck disable=SC1091
+# shellcheck source=init.sh
 source "init.sh"
 
-VENV_DIR="venv"
-PYTHON_PATH="$VENV_DIR/bin/python"
-
-check_dependency() {
-    if ! command -v "$1" &> /dev/null
-    then
-        log_error "$1 could not be found. Please install $1 to run this script."
-        exit
-    fi
-}
-
-setup_venv() {
-    if [ ! -d  "$VENV_DIR" ]
-    then
-        log_info "Creating virtual environment..."
-        python3 -m venv "$VENV_DIR"
-    fi
-}
-
-install_requirements() {
-    "$PYTHON_PATH" -m pip install -r "$DOTFILES_DIR/requirements.txt"
-}
-
-fetch_fonts() {
-    "$PYTHON_PATH" "$DOTFILES_SCRIPTS_DIR/fetch_fonts.py"
-}
-
 log_info "Starting fonts update..."
+DOTFILES_REQUIREMENTS="${DOTFILES_DIR}/requirements.txt"
+if [[ ! -f "${DOTFILES_REQUIREMENTS}" ]]; then
+    log_error "Requirements file (${DOTFILES_REQUIREMENTS}) not found..."
+    exit 1
+fi
+FETCH_FONTS_SCRIPT="${DOTFILES_SCRIPTS_DIR}/fetch_fonts.py"
+if [[ ! -f "${FETCH_FONTS_SCRIPT}" ]]; then
+    log_error "Fetch fonts script (${FETCH_FONTS_SCRIPT}) not found..."
+    exit 1
+fi
 
-# Check for necessary dependencies
-log_info "Checking for necessary dependencies..."
-for dep in "python3" "pip"
-do
-    check_dependency "$dep"
-done
+VENVS_DIR=${VENVS_DIR:-""}
+if [[ -z "$VENVS_DIR" || ! -d "$VENVS_DIR" ]]; then
+    log_error "VENVS_DIR not set or directory does not exists..."
+    exit 1
+fi
+DOTFILES_VENV_DIR="${VENVS_DIR}/dotfiles"
+if [[ ! -d "${DOTFILES_VENV_DIR}" ]]; then
+    mkdir -p "${DOTFILES_VENV_DIR}"
+fi
+PYTHON_CMD=$(command -v python3 || command -v python)
+if [[ -z "${PYTHON_CMD}" ]]; then
+    log_error "Python is not installed. Please install Python and run this script again."
+    exit 1
+fi
+DOTFILES_VENV_ACTIVATE="${DOTFILES_VENV_DIR}/bin/activate"
+if [[ ! -f "${DOTFILES_VENV_ACTIVATE}" ]]; then
+    log_info "Creating virtual environment for Fonts..."
+    "${PYTHON_CMD}" -m venv "${DOTFILES_VENV_DIR}"
+    echo "OK. Virtual environment created successfully!"
+fi
+# shellcheck disable=SC1090
+source "${DOTFILES_VENV_ACTIVATE}"
 
-# Set up Python virtual environment and install requirements
-log_info "Setting up Python virtual environment and installing requirements..."
-setup_venv
+PIP_CMD=$(command -v pip3 || command -v pip)
+if [[ -z "${PIP_CMD}" ]]; then
+    log_error "pip is not installed. Please install pip and run this script again."
+    deactivate
+    exit 1
+fi
 
-# Install requirements
-log_info "Installing requirements..."
-install_requirements
+# Install python requirements
+log_info "Installing python requirements (${DOTFILES_REQUIREMENTS})..."
+"${PIP_CMD}" install --upgrade pip && "${PIP_CMD}" install -r "${DOTFILES_REQUIREMENTS}"
+echo "OK. Python requirements installed."
 
 # Fetch fonts
 log_info "Fetching fonts..."
-fetch_fonts
+"${PYTHON_CMD}" "${FETCH_FONTS_SCRIPT}"
+echo "OK. Fonts fetched."
+
+# Deactivate venv
+log_info "Deactivating virtual environment..."
+deactivate
 
 log_success "Fonts update complete."
